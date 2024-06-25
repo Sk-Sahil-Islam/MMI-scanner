c094ccd064fa194ef9a96a05b751769a78e89285
sahil <sksahilislam0123@gmail.com>
2024-06-25 12:59:08 +0530

    made changes

--- main.py
+++ main.py
@@ -3,40 +3,36 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+import json
 
-# To make browser window automatically open, comments these 3 lines below (line 8,9,10)
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
+def getMmi():
+    # Initialize Chrome options
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
 
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service, options=chrome_options)
+    # Initialize the WebDriver
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
+    # Visit the target webpage
+    driver.get("https://edition.cnn.com/markets/fear-and-greed")
 
-print("Web Scraper is running...")
+    # Wait for the page to load
+    time.sleep(5)
 
-print("Loading the CNN Fear and Greed Index page...")
-driver.get("https://edition.cnn.com/markets/fear-and-greed")
-print(" ")
+    mmi = None
+    try:
+        # Locate the element containing the MMI value
+        mmi_result = driver.find_element(By.CLASS_NAME, "market-fng-gauge__historical-item-index-value")
+        mmi = int(mmi_result.text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Quit the WebDriver
+        driver.quit()
 
-time.sleep(5)
+    # Create the JSON response
+    response = json.dumps({ "mmi": mmi })
+    return response
 
-try:
-    mmi_result = driver.find_element(By.CLASS_NAME, "market-fng-gauge__historical-item-index-value")
-    mmi = int(mmi_result.text)
-    print(f"Market Mood Index (MMI) Score: {mmi}")
-except Exception as e:
-    print(f"An error occurred: {e}")
-
-if (0 <= mmi <= 24):
-    print("Sentiment: Extreme Fear")
-elif (25 <= mmi <= 44):
-    print("Sentiment: Fear")
-elif (44 < mmi <= 55):
-    print("Sentiment: Neutral")
-elif (55 < mmi <= 75):
-    print("Sentiment: Greed")
-elif (76 < mmi <= 100):
-    print("Sentiment: Extreme Greed")
-
-driver.quit()
-print("Program finished.")
+print(getMmi())
--- src/main.py
+++ src/main.py
@@ -1,18 +1,13 @@
 from appwrite.client import Client
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import json
+import time
 import os
 
-
-# This is your Appwrite function
-# It's executed each time we get a request
 def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
 
     # You can log messages to the console
     context.log("Hello, Logs!")
@@ -27,11 +22,38 @@ def main(context):
         return context.res.send("Hello, World!")
 
     # `ctx.res.json()` is a handy helper for sending JSON
+    mmi = getMmi()
     return context.res.json(
         {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
+            "mmi": mmi
         }
     )
+def getMmi():
+    # Initialize Chrome options
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+
+    # Initialize the WebDriver
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
+    # Visit the target webpage
+    driver.get("https://edition.cnn.com/markets/fear-and-greed")
+
+    # Wait for the page to load
+    time.sleep(5)
+
+    mmi = None
+    try:
+        # Locate the element containing the MMI value
+        mmi_result = driver.find_element(By.CLASS_NAME, "market-fng-gauge__historical-item-index-value")
+        mmi = int(mmi_result.text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Quit the WebDriver
+        driver.quit()
+
+    # Create the JSON response
+    response = json.dumps({ "mmi": mmi })
+    return response
